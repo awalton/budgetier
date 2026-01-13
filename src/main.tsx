d97ef09ba50e21479571ec266e76a3bf6e549369
awalton <78764965+awalton@users.noreply.github.com>
2026-01-13 10:24:59 -0500

    Generalized structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import { App } from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById('root');
+if (!root) throw new Error('No root element found');
+
+createRoot(root).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
---
+++ src/pages/splash/Splash.tsx
@@ -0,0 +1,16 @@
+const Splash = () => {
+  return (
+    <div className="container">
+      <section className="jumbotron">
+        <img
+          src="/images/budgetier-icon.svg"
+          alt="Budgetier logo, which consists of a stylizes wallet and mobile"
+        ></img>
+        <h1>Budgetier</h1>
+        <p>A home grown budgeting app</p>
+      </section>
+    </div>
+  );
+};
+
+export default Splash;
